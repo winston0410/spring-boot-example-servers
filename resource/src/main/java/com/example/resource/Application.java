6046360aa60f208f1130a3a602213e5390553511
Hugo Sum <hugosum@Hugos-MacBook-Pro.local>
2021-08-05 20:33:44 +0100

    Add hello world and create dockerfile for building

---
+++ resource/src/main/java/com/example/resource/Application.java
@@ -0,0 +1,31 @@
+package com.example.resource;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+// Add annotation for RequestMapping
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+
+@RestController
+@EnableAutoConfiguration
+public class Application {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
