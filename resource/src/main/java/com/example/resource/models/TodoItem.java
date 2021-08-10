8d11edb9e86ce9d0892b1162ef07472c68f7b135
Hugo Sum <hugosum@Hugos-MacBook-Pro.local>
2021-08-10 18:12:53 +0100

    Add hibernate-validator for validation

--- resource/src/main/java/com/example/resource/models/TodoItem.java
+++ resource/src/main/java/com/example/resource/models/TodoItem.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 
 enum Status {
     completed,
@@ -18,9 +19,12 @@ public class TodoItem {
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
     
+    @NotBlank
     private String name;
     
+    @NotBlank
     private String description;
     
+    @NotBlank
     private Status status;
 }
