d459573e92d3c58d1eb1599745e94c46c3f749c9
Hugo Sum <hugosum@Hugos-MacBook-Pro.local>
2021-08-11 15:05:07 +0100

    Create custom annotation for handling enum validation

--- resource/src/main/java/com/example/resource/type/req/TodoItem.java
+++ resource/src/main/java/com/example/resource/type/req/TodoItem.java
@@ -8,20 +8,21 @@ import javax.persistence.Id;
 import javax.persistence.Embeddable;
 import javax.validation.constraints.NotBlank;
 
-enum Status {
-    completed,
-    incomplete,
-    removed;
-}
+import com.example.resource.annotation.EnumNamePattern;
 
 @Embeddable
 public class TodoItem {
+    public enum Status {
+        completed,
+        incomplete,
+        removed;
+    }
     @NotBlank(message = "Name required")
     public String name;
     
     @NotBlank(message = "Description required")
     public String description;
     
-    // TODO: Validating enum
+    @EnumNamePattern(regexp = "completed|incomplete|removed")
     public Status status;
 }
